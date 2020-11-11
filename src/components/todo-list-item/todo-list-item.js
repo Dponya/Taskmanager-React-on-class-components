10041862b671cd16731eb6b538e0b134e806b33c
DaunPony <ealtynbek089@gmail.com>
2020-11-11 21:08:38 +0600

    Communication between components is improved

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -27,7 +27,7 @@ export class TodoListItem extends Component {
   }
 
   render() {
-    const { label } = this.props;
+    const { label, onDeleted } = this.props;
     const { done, important } = this.state;
 
     let classNames = 'todo-list-item';
@@ -52,7 +52,7 @@ export class TodoListItem extends Component {
         </button>
 
         <button type="button"
-          className="btn btn-outline-danger btn-sm float-right" >
+          className="btn btn-outline-danger btn-sm float-right" onClick={onDeleted}>
           <i className="fa fa-trash-o" />
         </button>
       </span>
