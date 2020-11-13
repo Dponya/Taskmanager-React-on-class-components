c5c812eb3019c459c642a71e34612bc5f3df478f
DaunPony <ealtynbek089@gmail.com>
2020-11-13 20:20:56 +0600

    Added form for buttonpanel. Its form can add new tasks on the Todo list

--- src/components/button-panel/button-panel.js
+++ src/components/button-panel/button-panel.js
@@ -1,14 +1,30 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 export class ButtonPanel extends Component {
+    state = {
+        label: '',
+    }
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    }
 
+    onSubmit = (e) => {
+        this.props.addItem(this.state.label);
+        e.preventDefault();
+    }
     render() {
-        return (<div>
-            <button type="button"
-                className="btn btn-outline-secondary"
-                onClick={() => this.props.addItem('HELLO WORLD')}
-            >Add the Task</button>
-        </div>
+        return (<form>
+            <input type="textarea" onChange={this.onLabelChange}>
+            </input>
+            <button type="submit"
+                className="btn btn-secondary"
+                onClick={this.onSubmit}>
+
+                Add new task
+                </button>
+        </form>
         );
     }
 }
